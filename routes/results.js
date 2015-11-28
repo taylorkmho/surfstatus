710162466c13a96cf87e7d70eaa4c9d0ffb3ee3c
Taylor Ho <taylorkmho@gmail.com>
2015-11-28 02:19:03 -1000

    BOARD check based on parameter

--- routes/results.js
+++ routes/results.js
@@ -8,14 +8,45 @@ router.get('/', function(req, res, next) {
 
   var quiver = req.query.quiver;
 
-  if (quiver) {
-    console.log('quiver exists and is ' + quiver);
-  } else {
+  if (quiver > 0) {
+    var quiverValues = quiver.split(''),
+        breaksBasedOnQuiver = [],
+        addBreakWith = function(boardType) {
+          surfbreaksList.forEach( function(surfbreak) {
+            if (
+                // [boardType] is in [surfbreak.boards]
+                surfbreak.boards.indexOf(boardType) > -1
+                &&
+                // [surfbreak] is NOT in [breaksBasedOnQuiver]
+                breaksBasedOnQuiver.indexOf(surfbreak) == -1
+            ) {
+              // push [surfbreak] to [breaksBasedOnQuiver]
+              breaksBasedOnQuiver.push(surfbreak);
+            }
+          });
+        };
+
+    if (quiverValues[0] > 0) {
+      addBreakWith('BB');
+    }
+    if (quiverValues[1] > 0) {
+      addBreakWith('SB');
+    }
+    if (quiverValues[2] > 0) {
+      addBreakWith('LB');
+    }
+    if (quiverValues[3] > 0) {
+      addBreakWith('SUP');
+    }
+
+  } else if (quiver < 1) {
+    console.log('YOU NEED A BOARD');
+  }
+  else {
     console.log('quiver does not exist');
   }
 
-  res.render('results', { title: 'Results', surfbreaks: surfbreaksList });
-
+  res.render('results', { title: 'Results', surfbreaks: breaksBasedOnQuiver });
 });
 
 module.exports = router;
