2fd581dbecaf12e5ca44582e6ebdad9c34ade282
Taylor Ho <taylorkmho@gmail.com>
2015-12-05 15:41:46 -1000

    Removed old routes

--- routes/skill.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('skill', { title: 'Pick your Skill Level' });
-});
-
-module.exports = router;
