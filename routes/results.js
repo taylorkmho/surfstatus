fe951f30fa7fb76409d28c0f6bea625136e14129
Taylor Ho <taylorkmho@gmail.com>
2015-12-06 01:27:37 -1000

    Using apicache
    that was easy

--- routes/results.js
+++ routes/results.js
@@ -1,13 +1,14 @@
 var express        = require('express');
 var request        = require('request');
 var async          = require('async');
+var apicache       = require('apicache').options({ debug: true }).middleware;
 var router         = express.Router();
 
 var surfbreaksList = require('../data/surfbreaks.json');
 var secrets        = require('../data/secrets.json');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', apicache('12 hours'), function(req, res, next) {
 
   /*
     MAGIC SEAWEED ACTION
