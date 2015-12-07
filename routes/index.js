373c12cd5ccd996066b2d408cc19ac447dd3bdeb
Taylor Ho <taylorkmho@gmail.com>
2015-12-07 00:34:40 -1000

    Added WEATHER report

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,33 @@ router.get('/', function(req, res, next) {
 
   var swellInfo = new Array();
   var surfHeightRanges = new Array();
+
+  var weatherAPIKey = secrets[0].apiKeys.openWeatherMap;
+
+  function toFarenheit(kelvin) {
+    return Math.round(kelvin * (9/5) - 459.67);
+  }
+
+  function toMPH(mps) {
+    return Math.round( (mps * 2.2369362920544) * 2 ) / 2;
+  }
+
+  function toCompass(deg) {
+    while ( deg < 0 ) deg += 360;
+    while ( deg >= 360 ) deg -= 360;
+    var val = Math.round( (deg -11.25 ) / 22.5 );
+    var arr = ["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
+    return arr[ Math.abs(val) ];
+  }
+
+  function toHITime(timestamp) {
+    var date = new Date(timestamp*1000);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    if ( hours > 12 ) { hours = hours - 12 };
+    return hours + ':' + minutes;
+  }
+
   async.parallel([
     function(callback) {
       request(
@@ -40,14 +67,46 @@ router.get('/', function(req, res, next) {
             outputRangeSplit['min'] = Math.round(outputRangeSplit[0] / hawaiianScale);
             outputRangeSplit['max'] = Math.round(outputRangeSplit[1] / hawaiianScale);
             outputRangeSplit['mean'] = (outputRangeSplit['min'] + outputRangeSplit['max'] / 2);
-            console.log('outputRangeSplit ' + outputRangeSplit);
+            // console.log('outputRangeSplit ' + outputRangeSplit);
             surfHeightRanges.push(outputRangeSplit);
           }
           callback();
 
         }
       )
-    }],
+    },
+    function(callback) {
+      // honolulu weather
+      request(
+        { url: "http://api.openweathermap.org/data/2.5/weather?id=5856195&appid=" + weatherAPIKey, method: "GET", timeout: 10000 },
+
+        function(err, response, body) {
+
+          if (err) {
+            res.render('error', {
+              message: err.message,
+              error: err
+            });
+          }
+          var jsonResponse = JSON.parse(body);
+          var weather = {
+            "temperatureMin" : toFarenheit(jsonResponse.main.temp_min),
+            "temperatureMax" : toFarenheit(jsonResponse.main.temp_max),
+            "clouds"         : jsonResponse.weather[0].description,
+            "windSpeed"      : toMPH(jsonResponse.wind.speed),
+            "windDir"        : jsonResponse.wind.deg,
+            "windDirComp"    : toCompass(jsonResponse.wind.deg),
+            "sunrise"        : toHITime(jsonResponse.sys.sunrise),
+            "sunset"         : toHITime(jsonResponse.sys.sunset)
+          };
+          console.log(weather);
+          callback();
+
+        }
+      )
+    }
+
+    ],
     function(err, results) {
       res.render('index', { title: 'Surf or Nah?', surfHeightRanges: surfHeightRanges, swellInfo: swellInfo });
     }
