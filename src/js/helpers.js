fe508ca32b5015ef53f4bed5480f6ca6fa2ddb11
Taylor Ho <taylorkmho@gmail.com>
2015-12-13 00:33:16 -1000

    Added wrap helper

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -13,3 +13,33 @@ function removeClass(el, className) {
     el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
   }
 }
+
+// Wrap an HTMLElement around each element in an HTMLElement array.
+HTMLElement.prototype.wrap = function(elms) {
+    // Convert `elms` to an array, if necessary.
+    if (!elms.length) elms = [elms];
+
+    // Loops backwards to prevent having to clone the wrapper on the
+    // first element (see `child` below).
+    for (var i = elms.length - 1; i >= 0; i--) {
+        var child = (i > 0) ? this.cloneNode(true) : this;
+        var el    = elms[i];
+
+        // Cache the current parent and sibling.
+        var parent  = el.parentNode;
+        var sibling = el.nextSibling;
+
+        // Wrap the element (is automatically removed from its current
+        // parent).
+        child.appendChild(el);
+
+        // If the element had a sibling, insert the wrapper before
+        // the sibling to maintain the HTML structure; otherwise, just
+        // append it to the parent.
+        if (sibling) {
+            parent.insertBefore(child, sibling);
+        } else {
+            parent.appendChild(child);
+        }
+    }
+};
